56c8ce1bf142989c9310f8956b5c2fe94b8127a1
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-15 16:49:17 +0300

    apply routes and order gates

--- main.go
+++ main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"microservice/app"
 )
 
 func main() {
-
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{ //& to store pointer
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	app := app.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to listen to server Error: ", err)
+		fmt.Println("Failed to start app: ", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
 }
